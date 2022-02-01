9699872f9b0a5c37369108c6219297728a7a65be
DCT <mirachan2332@gmail.com>
2022-02-01 15:25:56 -0600

    Employee.test.js constructor arguments added

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,12 +1,24 @@
+//ties Employee.js into Employee.test.js
+const { expect } = require('@jest/globals');
+const { test } = require('picomatch');
 const Employee = require('../lib/Employee.js');
 
-test('creates an Employee Object', () => {
-    const employee = new Employee('Charley');
+//constructor arguments
+test('should show employee name', () => {
+    const name = 'Dave'
+    const employee = new Employee(name)
+        expect(employee.name).toBe(name)
+})
 
-    expect(employee.name).toBe('Charley');
-    expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).toBe('Charley@Gmail.com');
+test('should show employee id', () => {
+    const id = 123456
+    const employee = new Employee("Dave", id)
+        expect(employee.id).toBe(id)
+})
 
-});
+test('should show employee email', () => {
+    const email = employee.name + "@gmail.com"
+    const employee = new Employee("Dave", 123456, email)
+        expect(employee.email).toBe(email)
+})
 
-//test for methods
