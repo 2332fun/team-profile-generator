9347717bb4062577dddfc99048ea3845a7144d89
DCT <mirachan2332@gmail.com>
2022-02-01 15:48:42 -0600

    finished method arguments for Employee.test.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,6 +1,4 @@
 //ties Employee.js into Employee.test.js
-const { expect } = require('@jest/globals');
-const { test } = require('picomatch');
 const Employee = require('../lib/Employee.js');
 
 //constructor arguments
@@ -17,8 +15,32 @@ test('should show employee id', () => {
 })
 
 test('should show employee email', () => {
-    const email = employee.name + "@gmail.com"
+    const email = 'employee.name + "@gmail.com"'
     const employee = new Employee("Dave", 123456, email)
         expect(employee.email).toBe(email)
 })
 
+//method arguments
+test('method for getting the employee name', () => {
+    const name = 'Dave'
+    const employee = new Employee(name)
+        expect(employee.getName()).toBe(name)
+})
+
+test('method for getting the employee id', () => {
+    const id = 123456
+    const employee = new Employee("Dave", id)
+        expect(employee.getID()).toBe(id)
+})
+
+test('method for getting the employee email', () => {
+    const email = 'employee.name + "@gmail.com"'
+    const employee = new Employee("Dave", 123456, email)
+        expect(employee.getEmail()).toBe(email)
+})
+
+test('method for getting the employee role', () => {
+    const role = 'Employee'
+    const employee = new Employee("Dave", 123456, 'email')
+        expect(employee.getRole()).toBe(role)
+})
