b3cfa101f95733f3081f6df252cd4d2984ad2bf5
DCT <mirachan2332@gmail.com>
2022-02-01 16:17:41 -0600

    intern.test.js implemented

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,22 @@
 //ties Intern.js into Intern.test.js
+const Intern = require('../lib/Intern.js');
+
+//constructor argument
+test('should show Intern school', () => {
+    const school = "remote/online"
+    const intern = new Intern("name", 123456, "email", school)
+        expect(intern.school).toBe(school)
+})
+
+//method arguments
+test('method for getting school', () => {
+    const school = "remote/online"
+    const intern = new Intern("name", 123456, "email", school)
+        expect(intern.getSchool()).toBe(school)
+})
+
+test('method for getting role', () => {
+    const getrole = 'Intern'
+    const intern = new Intern("name", 123456, "email", "school")
+    expect(intern.getRole()).toBe(getrole)
+})
