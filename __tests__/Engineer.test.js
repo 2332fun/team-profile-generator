056d88588d99044c6e8e2ffa079b8e066fec0bf3
DCT <mirachan2332@gmail.com>
2022-02-01 15:53:17 -0600

    constructor and method arguments for Engineer.test.js

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,23 @@
+const { expect } = require('@jest/globals');
+const { test } = require('jest-circus');
+const Engineer = require('../lib/Engineer.js');
+
+//constructor argument
+test('should show engineer github', () => {
+    const github = 'James'
+    const engineer = new Engineer("name", 123456, "email", github)
+        expect(engineer.github).toBe(github)
+})
+
+//method arguments
+test('method for getting engineer github', () => {
+    const github = 'James'
+    const engineer = new Engineer("name", 123456, "email", github)
+        expect(engineer.getGithub()).toBe(github)
+})
+
+test('method for getting engineer role', () => {
+    const getrole = 'Engineer'
+    const engineer = new Engineer("name", 123456, "email", "github")
+        expect(engineer.getRole()).toBe(getrole)
+})
