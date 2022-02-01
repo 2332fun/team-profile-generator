94e251b11b4a8a109b3b6974ffb4aa932ebaef1b
DCT <mirachan2332@gmail.com>
2022-02-01 16:05:25 -0600

    manager.js and manager.test.js

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,21 @@
+const Manager = require('../lib/Manager.js');
+
+//constructor argument
+test('should get office number', () => {
+    const officenumber = 654321
+    const manager = new Manager("name", 123456, "email", officenumber)
+        expect(manager.officeNumber).toBe(officenumber)
+})
+
+//method arguments
+test('method for getting office number', () => {
+    const officenumber = 654321
+    const manager = new Manager("name", 1234, "email", officenumber)
+        expect(manager.getOfficeNumber()).toBe(officenumber)
+})
+
+test('method for getting role', () => {
+    const getrole = 'Manager'
+    const manager = new Manager("name", 123456, "email", 654321)
+        expect(manager.getRole()).toBe(getrole)
+})
