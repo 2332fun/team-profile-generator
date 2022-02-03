b3de982e28b35af6af7440b8f65d660c45f43e30
DCT <mirachan2332@gmail.com>
2022-02-03 12:59:12 -0600

    removed unneccessary comments

--- src/page-template.js
+++ src/page-template.js
@@ -8,7 +8,7 @@ module.exports = generatePage => {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
+    <title>Team Profile Generator</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
     <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
@@ -62,58 +62,6 @@ const generateIntern = Intern => {
   <p>${internEmail}</p>
   <p>${internSchool}</p>`;
 }
-
-  
-  // create the projects section
-  // const generateProjects = projectsArr => {
-  //   return `
-  //     <section class="my-3" id="portfolio">
-  //       <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-  //       <div class="flex-row justify-space-between">
-  //       ${projectsArr
-  //         // .filter(({ feature }) => feature)
-  //         .map(({ name, description, languages, link }) => {
-  //           return `
-  //           <div class="col-12 mb-2 bg-dark text-light p-3">
-  //             <h3 class="portfolio-item-title text-light">${name}</h3>
-  //             <h5 class="portfolio-languages">
-  //               Built With:
-  //               ${languages.map(language => language).join(',')}
-  //             </h5>
-  //             <p>${description}</p>
-  //             <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-  //           </div>
-  //         `;
-  //         })
-  //         .join('')}
-  
-  //       ${projectsArr
-  //         // .filter(({ feature }) => !feature)
-  //         .map(({ name, description, languages, link }) => {
-  //           console.log(languages);
-  //           return `
-  //           <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-  //             <h3 class="portfolio-item-title text-light">${name}</h3>
-  //             <h5 class="portfolio-languages">
-  //               Built With:
-  //               ${languages.join(', ')}
-  //             </h5>
-  //             <p>${description}</p>
-  //             <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-  //           </div>
-  //         `;
-  //         })
-  //         .join('')}
-      
-  //       </div>
-  //     </section>
-  //   `;
-  // };
-  
-  // export function to generate entire page
-  // module.exports = templateData => {
-  //   // destructure page data by section
-  //   const { projects, about, ...header } = templateData;
   
 
   
