429f6aa400b1493d51c03562a9c0a57a8d6f76fe
DCT <mirachan2332@gmail.com>
2022-02-03 12:31:55 -0600

    removed old code from portfolio generator

--- src/page-template.js
+++ src/page-template.js
@@ -14,7 +14,11 @@ const generateAbout = aboutText => {
 
 // template for Manager
 const generateManager = managerId => {
-  return `<p>${managerId}</p>`;
+  return `
+  <p>${managerName}</p>
+  <p>${managerId}</p>
+  <p>${managerEmail}</p>
+  <p>${managerON}</p>`;
 }
 
 // template for intern
@@ -23,50 +27,50 @@ const generateManager = managerId => {
 
   
   // create the projects section
-  const generateProjects = projectsArr => {
-    return `
-      <section class="my-3" id="portfolio">
-        <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-        <div class="flex-row justify-space-between">
-        ${projectsArr
-          // .filter(({ feature }) => feature)
-          .map(({ name, description, languages, link }) => {
-            return `
-            <div class="col-12 mb-2 bg-dark text-light p-3">
-              <h3 class="portfolio-item-title text-light">${name}</h3>
-              <h5 class="portfolio-languages">
-                Built With:
-                ${languages.map(language => language).join(',')}
-              </h5>
-              <p>${description}</p>
-              <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-            </div>
-          `;
-          })
-          .join('')}
+  // const generateProjects = projectsArr => {
+  //   return `
+  //     <section class="my-3" id="portfolio">
+  //       <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+  //       <div class="flex-row justify-space-between">
+  //       ${projectsArr
+  //         // .filter(({ feature }) => feature)
+  //         .map(({ name, description, languages, link }) => {
+  //           return `
+  //           <div class="col-12 mb-2 bg-dark text-light p-3">
+  //             <h3 class="portfolio-item-title text-light">${name}</h3>
+  //             <h5 class="portfolio-languages">
+  //               Built With:
+  //               ${languages.map(language => language).join(',')}
+  //             </h5>
+  //             <p>${description}</p>
+  //             <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+  //           </div>
+  //         `;
+  //         })
+  //         .join('')}
   
-        ${projectsArr
-          // .filter(({ feature }) => !feature)
-          .map(({ name, description, languages, link }) => {
-            console.log(languages);
-            return `
-            <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-              <h3 class="portfolio-item-title text-light">${name}</h3>
-              <h5 class="portfolio-languages">
-                Built With:
-                ${languages.join(', ')}
-              </h5>
-              <p>${description}</p>
-              <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-            </div>
-          `;
-          })
-          .join('')}
+  //       ${projectsArr
+  //         // .filter(({ feature }) => !feature)
+  //         .map(({ name, description, languages, link }) => {
+  //           console.log(languages);
+  //           return `
+  //           <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+  //             <h3 class="portfolio-item-title text-light">${name}</h3>
+  //             <h5 class="portfolio-languages">
+  //               Built With:
+  //               ${languages.join(', ')}
+  //             </h5>
+  //             <p>${description}</p>
+  //             <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+  //           </div>
+  //         `;
+  //         })
+  //         .join('')}
       
-        </div>
-      </section>
-    `;
-  };
+  //       </div>
+  //     </section>
+  //   `;
+  // };
   
   // export function to generate entire page
   module.exports = templateData => {
